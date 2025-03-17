dcaacfce242697acf0383b82296aecd436979917
sedoisd <saraskin.serafim@yandex.ru>
2025-03-17 20:52:06 +0400

    решена проблема valueError ошибка iso format (не принимает | ввиде OR для запросов (filter))

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template, redirect
+import datetime
+
+import sqlalchemy
+from flask import Flask, render_template, redirect, make_response, request, session
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 from data import db_session
 from data.news import News
 from data.users import User
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+# app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+#     days=365
+# )
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 def main():
@@ -17,8 +36,59 @@ def main():
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.is_private != True)
+
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(sqlalchemy.or_(News.user == current_user, News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    # print(news, type(news), current_user)
     return render_template("index.html", news=news)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
 
 
 @app.route('/register', methods=['GET', 'POST'])
