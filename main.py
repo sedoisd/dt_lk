3cabd500554161515cde5e329116639d145ec8a5
sedoisd <saraskin.serafim@yandex.ru>
2025-03-18 15:05:49 +0400

    add edit delete news

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import datetime
 
 import sqlalchemy
-from flask import Flask, render_template, redirect, make_response, request, session
+from flask import Flask, render_template, redirect, make_response, request, session, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 from data import db_session
 from data.news import News
 from data.users import User
+from forms.news import NewsForm
 from forms.user import RegisterForm, LoginForm
 
 app = Flask(__name__)
@@ -16,10 +17,9 @@ login_manager.init_app(app)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-
-# app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
-#     days=365
-# )
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=31
+)
 
 
 @login_manager.user_loader
@@ -89,6 +89,73 @@ def cookie_test():
         res.set_cookie("visits_count", '1',
                        max_age=60 * 60 * 24 * 365 * 2)
     return res
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('news.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id,
+                                      News.user == current_user
+                                      ).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@app.route('/news', methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('news.html', title='Добавление новости',
+                           form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
